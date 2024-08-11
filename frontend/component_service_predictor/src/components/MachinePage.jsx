4f5b4f38259d9ed752321f654a0364ecb39bb2cc
GAUTHAM09012004 <gautham.s2022@vitstudent.ac.in>
2024-08-11 12:49:00 +0530

    feat:added pdf to frontend

--- frontend/component_service_predictor/src/components/MachinePage.jsx
+++ frontend/component_service_predictor/src/components/MachinePage.jsx
@@ -3,15 +3,13 @@ import { CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, Line
 import ReadData from './ReadData';
 import ReactToPrint from 'react-to-print';
 import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill } from 'react-icons/bs';
+import axios from 'axios';
 
 // Sample data
 const data = ReadData;
-console.log(ReadData);
-
 
 function MachinePage({ machine, onBack }) {
   const [selectedComponent, setSelectedComponent] = useState('');
-  const [showChatbot, setShowChatbot] = useState(false);
 
   // Extract unique components for the given machine
   const getUniqueComponents = () => {
@@ -64,9 +62,35 @@ function MachinePage({ machine, onBack }) {
   // Ref for print content
   const printRef = React.useRef();
 
-  const handleChatbotToggle = () => {
-    setShowChatbot(!showChatbot);
+  // Function to handle redirection
+  const handleRedirect = () => {
+    window.location.href = 'http://localhost:3000/';
   };
+  const handleDownloadPDF = async () => {
+    const token = localStorage.getItem('token'); // Get JWT token from local storage
+    if (!token) {
+      alert('No JWT token found');
+      return;
+    }
+  
+    try {
+      const response = await axios.get('http://localhost:3001/pdf/generate', {
+        headers: {
+          Authorization: `Bearer ${token}`, // Ensure 'Bearer' prefix is used for JWT
+        },
+        responseType: 'blob' // Ensure response is treated as binary data
+      });
+  
+      // Create a URL for the PDF blob and open it in a new tab
+      const url = URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }));
+      window.open(url);
+  
+    } catch (error) {
+      console.error('Error downloading PDF:', error);
+    }
+  };
+  
+  
 
   return (
     <main className='main-container'>
@@ -108,120 +132,109 @@ function MachinePage({ machine, onBack }) {
           <h1>42</h1>
         </div>
       </div>
-<div className='flex-row '>
-<div className="relative inline-block text-left">
-      <div className="flex justify-between items-center p-2 bg-gray-800 text-white rounded-md shadow-sm">
-        {/* <div className="text-sm font-semibold">Component:</div> */}
-        <div className="relative">
-          <select
-            value={selectedComponent}
-            onChange={(e) => setSelectedComponent(e.target.value)}
-            className="block w-full px-3 py-2 text-sm font-semibold text-gray-900 bg-white border border-gray-300 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-blue-500 focus:border-blue-500"
-          >
-            <option value="">Select a Component</option>
-            {getUniqueComponents().map(component => (
-              <option key={component} value={component}>
-                {component}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
-    </div>
 
-      <div className='chart-container'>
-        <div className='chart'>
-          {chartData.length > 0 ? (
-            <ResponsiveContainer width="100%" height={300}>
-              <LineChart
-                data={chartData}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
+      <div className='flex-row'>
+        <div className="relative inline-block text-left">
+          <div className="flex justify-between items-center p-2 bg-gray-800 text-white rounded-md shadow-sm">
+            <div className="relative">
+              <select
+                value={selectedComponent}
+                onChange={(e) => setSelectedComponent(e.target.value)}
+                className="block w-full px-3 py-2 text-sm font-semibold text-gray-900 bg-white border border-gray-300 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-blue-500 focus:border-blue-500"
               >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="Time" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                {allParameters.map(param => (
-                  <Line
-                    key={param}
-                    type="monotone"
-                    dataKey={param}
-                    stroke={parameterColors[param]}
-                  />
+                <option value="">Select a Component</option>
+                {getUniqueComponents().map(component => (
+                  <option key={component} value={component}>
+                    {component}
+                  </option>
                 ))}
-              </LineChart>
-            </ResponsiveContainer>
-          ) : (
-            <p>No data available for the selected component.</p>
-          )}
+              </select>
+            </div>
+          </div>
         </div>
 
-        <div className='controls'>
-          <ReactToPrint
-            trigger={() => <button className='pdf-button'>Download PDF</button>}
-            content={() => printRef.current}
-          />
-          <button className='back-button' onClick={onBack}>Back to Machines</button>
-        </div>
-      </div>
-
-      {/* Content to print */}
-      <div style={{ display: 'none' }}>
-        <div ref={printRef}>
-          <h1>{machine} Report</h1>
-          <div className='charts'>
+        <div className='chart-container'>
+          <div className='chart'>
             {chartData.length > 0 ? (
-              <LineChart
-                data={chartData}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-                width={800}
-                height={400}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="Time" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                {allParameters.map(param => (
-                  <Line
-                    key={param}
-                    type="linear"
-                    dataKey={param}
-                    stroke={parameterColors[param]}
-                  />
-                ))}
-              </LineChart>
+              <ResponsiveContainer width="100%" height={300}>
+                <LineChart
+                  data={chartData}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="Time" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  {allParameters.map(param => (
+                    <Line
+                      key={param}
+                      type="monotone"
+                      dataKey={param}
+                      stroke={parameterColors[param]}
+                    />
+                  ))}
+                </LineChart>
+              </ResponsiveContainer>
             ) : (
               <p>No data available for the selected component.</p>
             )}
           </div>
-        </div>
-      </div>
 
-      {/* Chatbot */}
-      {showChatbot && (
-        <div className='chatbot'>
-          <h3>Chatbot</h3>
-          <div className='chatbot-messages'>
-            {/* Chatbot messages will go here */}
+          <div className='controls'>
+            <ReactToPrint
+              trigger={() => <button className='pdf-button'>Download Graph</button>}
+              content={() => printRef.current}
+            />
+            <button className='redirect-button' onClick={handleRedirect}>Talk with ChatBot</button>
+            <button className='download-button' onClick={handleDownloadPDF}>Download PDF </button>
+
+            <button className='back-button' onClick={onBack}>Back to Machines</button>
           </div>
-          <input type='text' placeholder='Ask a question...' />
         </div>
-      )}
-      <button onClick={handleChatbotToggle} className='chatbot-toggle'>
-        {showChatbot ? 'Close Chatbot' : 'Open Chatbot'}
-      </button>
+
+        {/* Content to print */}
+        <div style={{ display: 'none' }}>
+          <div ref={printRef}>
+            <h1>{machine} Report</h1>
+            <div className='charts'>
+              {chartData.length > 0 ? (
+                <LineChart
+                  data={chartData}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                  width={800}
+                  height={400}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="Time" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  {allParameters.map(param => (
+                    <Line
+                      key={param}
+                      type="linear"
+                      dataKey={param}
+                      stroke={parameterColors[param]}
+                    />
+                  ))}
+                </LineChart>
+              ) : (
+                <p>No data available for the selected component.</p>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
